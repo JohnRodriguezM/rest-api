3fb27305710adfd74fedf715732eb02c6f0e347a
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-05-26 20:24:34 -0500

    h

---
+++ src/firebasee/script.mjs
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChIfO6vB8O6ZXDmV5PNb7faVcX4IfBDvQ",
+  authDomain: "dogg-f7361.firebaseapp.com",
+  projectId: "dogg-f7361",
+  storageBucket: "dogg-f7361.appspot.com",
+  messagingSenderId: "420282308981",
+  appId: "1:420282308981:web:be5836f50daef950ca5b3c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log('soyssss', app)
